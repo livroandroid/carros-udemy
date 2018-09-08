4666c68a0f7af33bc66188ded42d929f0df58d1e
Ricardo Lecheta <rlecheta@livetouch.com.br>
2018-09-08 19:34:20 -0300

    12-xml

--- app/src/main/java/br/com/livroandroid/carros/domain/CarroService.kt
+++ app/src/main/java/br/com/livroandroid/carros/domain/CarroService.kt
@@ -1,21 +1,54 @@
 package br.com.livroandroid.carros.domain
 
 import android.content.Context
+import android.util.Log
+import br.com.livroandroid.carros.R
+import br.com.livroandroid.carros.extensions.getText
+import br.com.livroandroid.carros.extensions.getXml
 
 class CarroService {
     companion object {
-        // Busca os carros por tipo
+        private const val TAG = "livro"
+
+        // Busca os carros por tipo (clássicos, esportivos ou luxo)
         fun getCarros(context: Context, tipo: TipoCarro): List<Carro> {
-            val tipoString = context.getString(tipo.string)
+            // Este é o arquivo que temos que ler
+            val raw = getArquivoRaw(tipo)
+            // Abre o arquivo para leitura
+            val resources = context.resources
+            val inputStream = resources.openRawResource(raw)
+            inputStream.bufferedReader().use {
+                // Lê o XML e cria a lista de carros
+                val xml = it.readText()
+                return parserXML(xml)
+            }
+        }
+
+        // Retorna o arquivo que temos que ler para o tipo informado
+        private fun getArquivoRaw(tipo: TipoCarro) = when (tipo) {
+            TipoCarro.Classicos -> R.raw.carros_classicos
+            TipoCarro.Esportivos -> R.raw.carros_esportivos
+            else -> R.raw.carros_luxo
+        }
+
+        // Lê o XML e cria a lista de carros
+        private fun parserXML(xmlString: String): List<Carro> {
             val carros = mutableListOf<Carro>()
-            for (i in 1..100) {
+            val xml = xmlString.getXml()
+            // Lê todas as tags <carro>
+            val nodeCarros = xml.getChildren("carro")
+            // Insere cada carro na lista
+            for (node in nodeCarros) {
                 val c = Carro()
-                c.nome = "Carro $tipoString: $i"
-                c.desc = "Desc $i"
-                c.urlFoto = "http://www.livroandroid.com.br/livro/carros/esportivos/Ferrari_FF.png"
+                // Lê as informações de cada carro
+                c.nome = node.getText("nome")
+                c.desc = node.getText("desc")
+                c.urlFoto = node.getText("url_foto")
                 carros.add(c)
             }
+            Log.d(TAG, "${carros.size} carros encontrados.")
             return carros
         }
+
     }
 }
